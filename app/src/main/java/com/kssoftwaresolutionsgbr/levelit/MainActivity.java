52ee14aab70e9ac2625249f2877d4f698422ec6b
Malte Schönert <malte.schoenert@outlook.de>
2021-02-08 16:49:23 +0100

    new Textview in MainActivity showing the current alignment angle calculation is still missing

--- app/src/main/java/com/kssoftwaresolutionsgbr/levelit/MainActivity.java
+++ app/src/main/java/com/kssoftwaresolutionsgbr/levelit/MainActivity.java
@@ -4,29 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // fields
     private Accelerometer accelerometer;
+    private CalculatorLocal calculatorLocal;
+
+    TextView tv_alignment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv_alignment = (TextView)findViewById(R.id.tv_alignment);
+
         accelerometer = new Accelerometer(this);
+        calculatorLocal = new CalculatorLocal();
 
         accelerometer.setListener(new Accelerometer.Listener() {
             @Override
             public void onTranslation(float tx, float ty, float tz) {
-                // testing of acc sensor
+                calculatorLocal.get_data(tx, ty, tz);
+
+                if(calculatorLocal.alignment == Alignment.UPWARD){
+                    tv_alignment.setText("upward");
+                }
+                else if (calculatorLocal.alignment == Alignment.DOWNWARD){
+                    tv_alignment.setText("downward");
+                }
+                else if (calculatorLocal.alignment == Alignment.RIGHTWARD){
+                    tv_alignment.setText("rightward");
+                }
+                else if (calculatorLocal.alignment == Alignment.LEFTWARD){
+                    tv_alignment.setText("leftward");
+                }
+
+                /* testing of acc sensor
                 if(tx > 1.0f){
                     getWindow().getDecorView().setBackgroundColor(Color.RED);
                 }
                 else if(tx < -1.0f){
                     getWindow().getDecorView().setBackgroundColor(Color.BLUE);
                 }
+                */
             }
         });
     }
